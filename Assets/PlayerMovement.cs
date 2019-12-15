4b9b5f05a30829dc0bb8298027b92399452302e6
blinded-famas <alex.blinded@gmail.com>
2019-12-16 00:55:44 +0300

    add collisions, camera follow, score counter, spawning

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,11 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public float movementSpeed = 500f;
 
     
     void FixedUpdate()
     {
         rb.useGravity = false;
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, movementSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/SpawnHoops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHoops : MonoBehaviour
+{
+    public GameObject[] hoops;
+    public Vector3 offset;
+    public GameObject cameraObj;
+    public float respawnTime = 0.25f;
+
+    public void Start()
+    {
+        StartCoroutine(HoopsSpawningCoroutine());
+    }
+    void CreateHoop()
+    {
+        int hoops_num = Random.Range(0, 3);
+        offset = new Vector3(0, 0, Random.Range(15f, 20f));
+        Vector3 pos = new Vector3(0,0,cameraObj.transform.position.z) + offset;
+        GameObject createdHoop;
+        createdHoop = Instantiate(hoops[hoops_num], pos, hoops[hoops_num].transform.rotation);
+    }
+
+    IEnumerator HoopsSpawningCoroutine() 
+    { 
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            CreateHoop();
+        }
+        
+    }
+}
