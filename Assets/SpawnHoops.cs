c08d0382e29d1a83a21b2d75d8d35d4b85e947a0
blinded-famas <alex.blinded@gmail.com>
2019-12-29 21:50:41 +0300

    add fix for spawning

--- Assets/SpawnHoops.cs
+++ Assets/SpawnHoops.cs
@@ -8,24 +8,54 @@ public class SpawnHoops : MonoBehaviour
     public GameObject[] hoops;
     public Vector3 offset;
     public Vector3 vec;
+    public GameObject player;
+    GameObject spawnedHoop;
+    List<GameObject> createdHoops = new List<GameObject>();
+    
 
-    void SpawnObjects()
+    public void SpawnObjects()
     {
-        for (int i = 0; i < 15; i++)
+        //create objects
+        for (int i = 0; i < 10; i++)
         {
+            int hoops_num = Random.Range(0, hoops.Length);
+            offset = new Vector3(0, 0, Random.Range(15f, 25f));
+            Vector3 pos = transform.position + offset + vec;
+            spawnedHoop = Instantiate(hoops[hoops_num], pos, hoops[hoops_num].transform.rotation);
+            vec = spawnedHoop.transform.position;
+            //add to list
+            createdHoops.Add(spawnedHoop);
+        }
+        
+    }
+
+    public void DestroyAndCreateNewHoops()
+    {
+        //compare positions of hoop and player
+        if ((createdHoops[0].transform.position.z + 5) < player.transform.position.z)
+        {
+            //destroy hoops
+            Destroy(createdHoops[0]);
+            createdHoops.RemoveAt(0);
+            //and create new
             int hoops_num = Random.Range(0, hoops.Length);
             offset = new Vector3(0, 0, Random.Range(20f, 30f));
             Vector3 pos = transform.position + offset + vec;
-            GameObject spawnedHoop = Instantiate(hoops[hoops_num], pos, hoops[hoops_num].transform.rotation);
+            spawnedHoop = Instantiate(hoops[hoops_num], pos, hoops[hoops_num].transform.rotation);
             vec = spawnedHoop.transform.position;
+            //add to list
+            createdHoops.Add(spawnedHoop);
         }
     }
-
     
-    public void Update()
+    public void Start()
     {
         SpawnObjects();
-        
+    }
+
+    public void Update()
+    {
+        DestroyAndCreateNewHoops();
     }
 
 
